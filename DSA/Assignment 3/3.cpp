05e1ec1e5695ecaefbfeaab7d98ffe9879a6a099
nioomeee <nioomeee@gmail.com>
2025-03-25 20:10:54 +0530

    stack using ll

--- DSA/Assignment 3/3.cpp
+++ DSA/Assignment 3/3.cpp
@@ -24,8 +24,9 @@ public:
     }
     void push_back(int val) {
         Node* newNode = new Node(val);
-        if( head == tail == NULL) {
+        if( head == NULL) {
             head = tail = newNode;
+            return;
         }
         tail->next = newNode;
         tail = newNode;
@@ -40,9 +41,57 @@ public:
         }
         cout << "NULL" << endl;
     }
+
+    void pop_back() {
+        if(isEmpty()) {
+            cout <<"empty" <<endl;
+            return;
+        }
+        Node* temp = head;
+        while(temp->next != tail){
+            temp = temp->next;
+        }
+        temp->next = NULL;
+        delete tail;
+        tail = temp;
+                
+    }
+
+    bool isEmpty() {
+        return head == NULL;
+    }
 };
 
 class Stack {
 public: 
-    
+
+    List l;
+    void push(int val){
+        l.push_back(val);
+    }
+
+    void pop() {
+        l.pop_back();
+    }
+
+    void print() {
+        l.printLL();
+    }
 };
+
+int main() {
+    Stack s;
+    s.push(5);
+    s.push(6);
+    s.push(7);
+    s.push(8);
+    s.push(9);
+
+    s.print();
+
+    s.pop();
+
+    s.print();
+
+    return 0;
+}
