13f62e54f39e52666b9593df9c21c9da9e79f30d
nioomeee <nioomeee@gmail.com>
2025-04-11 00:14:59 +0530

    applet circle and name

--- Java/Assignment-5/q8.java
+++ Java/Assignment-5/q8.java
@@ -5,7 +5,25 @@ import java.applet.Applet;
 import java.awt.*;
 
 public class q8 extends Applet {
-    public static void main(String[] args) {
+
+    // main func for applet
+    public void paint(Graphics g){
+        g.setColor(Color.RED);
         
+        int width = 500;
+        int height = 500;
+
+        int radius = 100;
+
+        int centreX = width / 2;
+        int centreY = height / 2;
+
+        int topLeftX = centreX - radius;
+        int topLeftY = centreY - radius;
+
+        g.drawOval(topLeftX, topLeftY, 2*radius, 2 * radius);
+        
+        g.setColor(Color.PINK);
+        g.drawString("Niomi", centreX-15, centreY+5);
     }
 }
